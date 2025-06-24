6259d0f7ef601c21a3f2110729fa13ba31156d4b
YASH <yashchaurasia630@gmail.com>
2025-06-24 16:50:22 +0530

    encoder update

--- src/encoder/encoder.go
+++ src/encoder/encoder.go
@@ -4,49 +4,140 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
-func main() {
-	announce := "udp://tracker.openbittorrent.com:80/announce"
+type File struct {
+	length int64
+	path   []string
+}
+
+type Info struct {
+	name        string
+	pieceLength int
+	pieces      []byte
+	length      int64
+	files       []File
+}
+
+type Torrent struct {
+	announce         string
+	announceList     []string
+	createdBy        string
+	creationDate     int64
+	encoding         string
+	comment          string
+	hasMultipleFiles bool
+	info             Info
+}
+
+var meta = Torrent{
+	announce:         "udp://tracker.openbittorrent.com:80/announce",
+	announceList:     []string{},
+	createdBy:        "",
+	creationDate:     time.Now().Unix(),
+	encoding:         "UTF-8",
+	comment:          "",
+	hasMultipleFiles: false,
+	info: Info{
+		name:        "",
+		pieceLength: 2,
+		pieces:      []byte{},
+		length:      0,
+		files:       []File{},
+	},
+}
+
+func getDetails() {
+	var path string
+
 	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Printf("Announce URL [default: %s]: ", announce)
-	fmt.Scanln(&announce)
+	fmt.Printf("Announce URL [default: %s]: ", meta.announce)
+	fmt.Scanln(&meta.announce)
 
-	fmt.Printf("Author: ")
-	author, _ := reader.ReadString('\n')
-	author = strings.TrimSpace(author)
-
-	creationDate := time.Now().Unix()
-	encoding := "UTF-8"
-
-	fmt.Println("Comment [default: \"\"]: ")
-	comment, _ := reader.ReadString('\n')
-	comment = strings.TrimSpace(comment)
-
-	var name string
+	fmt.Println("Announce List [default: []]: ")
 	for {
-		fmt.Println("Name of the file [essential]: ")
+		var input string
+		fmt.Scanln(&input)
+
+		if input == "" {
+			break
+		}
+	}
+
+	fmt.Print("Author: ")
+	meta.createdBy, _ = reader.ReadString('\n')
+	meta.createdBy = strings.TrimSpace(meta.createdBy)
+
+	fmt.Print("Comment [default: \"\"]: ")
+	meta.comment, _ = reader.ReadString('\n')
+	meta.comment = strings.TrimSpace(meta.comment)
+
+	for {
+		fmt.Print("Name of the file [essential]: ")
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println("Error reading input: ", err)
 			continue
 		}
-		name = strings.TrimSpace(input)
+		meta.info.name = strings.TrimSpace(input)
 
-		if name != "" {
+		if meta.info.name != "" {
 			break
 		}
 	}
 
-	pieceSize := 2
-	fmt.Printf("Piece Size [default: %dMB]: ", &pieceSize)
-	_, err := fmt.Scan(&pieceSize)
+	fmt.Printf("Piece Size [default: %dMB]: ", meta.info.pieceLength)
+	inp, err := reader.ReadString('\n')
 
 	if err != nil {
-		fmt.Println("Invalid input:", err)
+		fmt.Println("Error reading input:", err)
+		return
 	}
 
+	inp = strings.TrimSpace(inp)
+	if inp != "" {
+		uInp, err := strconv.Atoi(inp)
+		if err != nil {
+			fmt.Println("Invalid number, using default:", meta.info.pieceLength)
+		} else {
+			meta.info.pieceLength = uInp
+		}
+	}
+
+	for {
+		fmt.Print("Path to the file [essential]: ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input: ", err)
+			continue
+		}
+		path = strings.TrimSpace(input)
+
+		if path != "" {
+			break
+		}
+	}
+
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		fmt.Println("Path does not exist")
+	} else if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	if info.IsDir() {
+		fmt.Println("The given path is a directory.")
+	} else {
+		fmt.Println("The given path is a file.")
+	}
+
+}
+
+func main() {
+	getDetails()
 }
